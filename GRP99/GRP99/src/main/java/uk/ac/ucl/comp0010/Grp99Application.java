a2a14cca26494d14097e21f5e6481feb18e0d960
Ashtonhii <ashtonhii27@gmail.com>
2024-11-21 15:22:50 +0000

    new project

---
+++ GRP99/GRP99/src/main/java/uk/ac/ucl/comp0010/Grp99Application.java
@@ -0,0 +1,13 @@
+package uk.ac.ucl.comp0010;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grp99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grp99Application.class, args);
+	}
+
+}
